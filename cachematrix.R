6bd7f4fa38761c943e608dea8d8823376832f606
vpnet <rp_vpnet@yahoo.com>
2017-11-24 07:01:16 +0000

    2nd commmit

--- cachematrix.R
+++ cachematrix.R
@@ -4,12 +4,37 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  # initialize the cache
+  cache <- NULL
+  # implement set, get, inverse functions
+  set <- function(y) {
+    x <<- y
+    # flush cache
+    cache <<- NULL
+  }
+  # just return stored value
+  get <- function() x
+  
+  setinv <- function(z) cache <<- z
+  
+  # get the cache value
+  getinv <- function() cache
+  
+  # return a list
+  list(set = set, get = get, setinv = setinv, getinv = getinv)
 }
 
 
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  cache <<- x$getinv()
+  
+  if(!is.null(cache)){
+    return(cache)
+  }
+  mat <- x$get()
+  cache <- solve(mat, ...)
+  x$setinv(cache)
+  cache
 }
